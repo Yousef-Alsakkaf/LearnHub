4c6956463f6a05d13295276686c0ce6b328bfdc3
Abdulrahman Alali <22000577@ug.buid.ac.ae>
2024-03-17 02:00:48 +0400

    Added socket.tsx

---
+++ Frontend/src/socket.tsx
@@ -0,0 +1,16 @@
+import io from "socket.io-client"
+
+const isProduction = process.env.NODE_ENV === 'production';
+const serverURL = isProduction ? "https://shelfmate-mcfs.onrender.com" : "http://localhost:4000";
+const socket = io(serverURL);
+
+socket.on("connect", () => {
+    console.log("Connected to server!");
+})
+
+socket.on("disconnect", () => {
+    window.location.href = "/";
+    window.location.reload();
+})
+
+export default socket;
