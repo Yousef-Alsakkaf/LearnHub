a80e9150d597974196ccf623fa41fc6c1d7c0d59
waf-iq <wafiqdakens@gmail.com>
2024-03-17 02:28:37 +0400

    sql changes

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -2,6 +2,9 @@ CREATE TABLE `users` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `username` varchar(50),
   `password` varchar(50),
+  `fName` varchar(50),
+  `lName` varchar(50),
+  `UID` varchar(8),
   `type` ENUM('student','instructor','admin'),
   `email` varchar(255),
   `active` BOOLEAN
@@ -11,24 +14,6 @@ CREATE TABLE access_tokens (
 id INT,
 token varchar(120),
 FOREIGN KEY (`id`) REFERENCES `users` (`id`)
-);
-
-CREATE TABLE `students` (
-  `id` INT,
-  `fName` varchar(50),
-  `lName` varchar(50),
-  `UID` varchar(8),
-  PRIMARY KEY (`UID`),
-  FOREIGN KEY (`id`) REFERENCES `users` (`id`)
-);
-
-CREATE TABLE `instructor` (
-  `id` INT,
-  `fName` varchar(50),
-  `lName` varchar(50),
-  `UID` VARCHAR(8),
-  PRIMARY KEY (`UID`),
-  FOREIGN KEY (`id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE `courses` (
@@ -44,14 +29,14 @@ CREATE TABLE `studies` (
   `final_grade` FLOAT,
   `attendance` FLOAT,
   FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
-  FOREIGN KEY (`student_id`) REFERENCES `students` (`id`)
+  FOREIGN KEY (`student_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE `teaches` (
   `course_id` INT,
   `instructor_id` INT,
   FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
-  FOREIGN KEY (`instructor_id`) REFERENCES `instructor` (`id`)
+  FOREIGN KEY (`instructor_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE `material` (
@@ -63,12 +48,6 @@ CREATE TABLE `material` (
   FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
 );
 
-CREATE TABLE `admin` (
-  `id` INT,
-  `fName` varchar(50),
-  `lName` varchar(50),
-  FOREIGN KEY (`id`) REFERENCES `users` (`id`)
-);
 
 CREATE TABLE `m_grade` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
@@ -78,12 +57,12 @@ CREATE TABLE `m_grade` (
   `grade` FLOAT,
   FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
   FOREIGN KEY (`material_id`) REFERENCES `material` (`id`),
-  FOREIGN KEY (`student_id`) REFERENCES `students` (`id`)
+  FOREIGN KEY (`student_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE `majors` (
   `name` varchar(50),
-  PRIMARY KEY  (`name`)
+   PRIMARY KEY  (`name`)
 );
 
 CREATE TABLE `requests` (
