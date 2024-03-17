3bf5b42ea492a6769b9609245724ddf93afbd5af
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-03-17 22:18:07 +0400

    set up the course info page

--- Frontend/src/components/modal/ViewMonal.tsx
+++ Frontend/src/components/modal/ViewMonal.tsx
@@ -1,29 +1,32 @@
+import React from "react";
+
 interface ModalProps {
   isVisible: boolean;
   onClose: () => void;
-  children: any;
+  children: React.ReactNode;
 }
 
-const ViewModal = ({ isVisible, onClose, children }: ModalProps) => {
+const ViewModal: React.FC<ModalProps> = ({ isVisible, onClose, children }: ModalProps) => {
   if (!isVisible) return null;
 
-  const handleClose = (e: any) => {
-    if (e.target.id === "wrapper") onClose();
+  const handleClose = () => {
+    onClose();
   };
+
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center lg:ml-[200px]"
       id="wrapper"
       onClick={handleClose}
     >
-      <div className="w-[600px] flex flex-col">
+      <div className="bg-white p-4 rounded-lg relative" style={{ width: '70%', height: '80%' }}>
         <button
-          className="text-white text-xl place-self-end"
-          onClick={() => onClose()}
+          className="text-gray-600 text-xl absolute top-2 right-2"
+          onClick={handleClose}
         >
           X
         </button>
-        <div className="bg-white p-2 rounded">{children}</div>
+        {children}
       </div>
     </div>
   );
