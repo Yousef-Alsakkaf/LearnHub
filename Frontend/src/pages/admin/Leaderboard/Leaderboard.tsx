788a9d9dd7e3b26913fd97795ae0d7d68629e3b8
yamanoos <22001064@ug.buid.ac.ae>
2024-03-18 01:15:14 +0400

    insert_users2 and insert_courses2

--- Frontend/src/pages/admin/Leaderboard/Leaderboard.tsx
+++ Frontend/src/pages/admin/Leaderboard/Leaderboard.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+import "../../../styles/Leaderboard.css"
 
 
 function Leaderboard() {
   return (
     <div>
         <h1>Leaderboard</h1>
+        <div className="leaderboard">
+
+        </div>
     </div>
   )
 }
---
+++ SQL/insert_courses2.sql
@@ -0,0 +1,16 @@
+SELECT * FROM learnhub.courses;
+
+INSERT INTO courses (title, course_code, academic_year)
+VALUES 
+('Database Management', 'CS201', 2024),
+('Web Development', 'CS301', 2024),
+('Mobile Application Development', 'CS1201', 2024),
+('Information Retrieval', 'CS1301', 2024),
+('Cloud Computing', 'CS1401', 2024),
+('Computer Networks', 'CS401', 2024),
+('Machine Learning', 'CS501', 2024),
+('Software Engineering', 'CS601', 2024),
+('Operating Systems', 'CS701', 2024),
+('Cybersecurity', 'CS801', 2024),
+('Artificial Intelligence', 'CS1001', 2024);
+
