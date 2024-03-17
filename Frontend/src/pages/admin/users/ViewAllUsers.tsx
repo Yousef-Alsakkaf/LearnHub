2782dccbea1af9b3f49c42fe7a07bb05abf7f89f
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-03-17 20:44:00 +0400

    mionor fix

--- Frontend/src/pages/admin/users/ViewAllUsers.tsx
+++ Frontend/src/pages/admin/users/ViewAllUsers.tsx
@@ -63,7 +63,7 @@ function AllUsers() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {students.map((student) => (
+        {students && students.map((student) => (
           <div key={student.id} className="max-w-sm">
             <div className="group rounded-lg border bg-white p-4 shadow-lg transition-transform transform hover:-translate-y-1 hover:shadow-xl">
               <div className="relative mx-auto w-36 h-36 rounded-full overflow-hidden">
