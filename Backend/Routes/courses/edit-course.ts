84b90c5e8abb9877453ea652045bcfc8ce3045e2
waf-iq <wafiqdakens@gmail.com>
2024-03-22 17:36:41 +0400

    added edit course

---
+++ Backend/Routes/courses/edit-course.ts
@@ -0,0 +1,56 @@
+import { error } from "ajv/dist/vocabularies/applicator/dependencies.js";
+import { ServerCommandBuilder } from "../../Applications/Commands/Builder.js";
+import { UserAccessLevels, CommandExecuteArguments } from "../../Applications/Commands/Context.js";
+import { v4 as uuid } from "uuid";
+
+const command = new ServerCommandBuilder("edit-course")
+  .setAccessLevel(UserAccessLevels.ADMIN)
+  .setOutgoingChannel("edit-course-response")
+  .setIncomingValidationSchema({
+        type: "object",
+        additionalProperties: false,
+        properties: {
+            id: { type: "number" },
+            title: { type: "string" },
+            course_code: { type: "string" },
+            academic_year: { type: "string" },
+            image: { type: "string" },
+            description: { type: "string" },
+        },required:["id","title","course_code","academic_year","image","description"]       
+      })
+  .setExecute(callback)
+  .setOutgoingValidationSchema({})
+  .build();
+
+async function callback({ Client, Data, Database }: CommandExecuteArguments) {
+
+    try {
+        const Userid=Client.getId();
+        const user=Client.getName();
+        const {title,course_code,academic_year,image,description}=Data;
+        await Database.executeQuery('UPDATE courses SET title=?,course_code=?,academic_year=?,image=?,description=? WHERE id=?',[title,course_code,academic_year,image,description,Data.id]);
+        await Database.createLog({ event: "Add course", details: `User ${user} added course ${title}`, initiator: Userid });
+        return{
+              notification: {
+                type: "success",
+                message: "Course Added successfully!",
+                },
+              error: false,
+        };
+        
+    } catch (error) {
+
+        return {
+            notification: {
+              type: "error",
+              message: "Failed to edit course!",
+            },
+            error: true,
+          };
+        
+    }
+  
+
+}
+
+export default command;
