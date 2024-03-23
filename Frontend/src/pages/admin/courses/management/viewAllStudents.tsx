e92d79fcae3d3462fd89c17c7aa899eaa96a6033
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-03-23 16:59:03 +0400

    minor change

--- Frontend/src/pages/admin/courses/management/viewAllStudents.tsx
+++ Frontend/src/pages/admin/courses/management/viewAllStudents.tsx
@@ -1,51 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import socket from '@/socket';
+import React, { useEffect, useState } from "react";
+import socket from "@/socket";
 
 interface User {
-    UID: string;
-    username: string;
-    fName: string;
-    lName?: string;
+  type: string;
+  fName: string;
+  lName?: string;
 }
 
 const ViewAllStudents = () => {
-    const [students, setStudents] = useState<User[]>([]);
+  const [students, setStudents] = useState<User[]>([]);
 
-    useEffect(() => {
-        socket.emit("get-all-users", {});
-        socket.on("get-all-users-response", (response: User[]) => {
-            console.log("this is the response from get-all-users", response);
-            setStudents(response); 
-        });
+  useEffect(() => {
+    socket.emit("get-course-roaster", { id: 1 });
+    socket.on("get-course-roaster-response", (response: User[]) => {
+      console.log("this is the response from get-users from roster", response);
+      setStudents(response);
+    });
 
-        return () => {
-            socket.off("get-all-users-response");
-        };
-    }, []);
+    return () => {
+      socket.off("get-all-users-response");
+    };
+  }, []);
 
-    return (
-        <div>
-            <h1>All Students</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>UID</th>
-                        <th>Username</th>
-                        <th>Full Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {students.map((student) => (
-                        <tr>
-                            <td>{student.UID}</td>
-                            <td>{student.username}</td>
-                            <td>{`${student.fName} ${student.lName || ''}`}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">All Students</h1>
+      <table className="table-auto w-full">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="px-4 py-2">User type</th>
+            <th className="px-4 py-2">Full Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student, index) => (
+            <tr key={index} className="hover:bg-gray-100 transform hover:scale-105 transition duration-300 ease-in-out">
+              <td className="border px-4 py-2">{student.type}</td>
+              <td className="border px-4 py-2">{`${student.fName} ${student.lName || ""}`}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ViewAllStudents;
