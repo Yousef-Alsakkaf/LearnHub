561a5289b9169da264b2d6a732f26d90f53eea1e
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-03-23 23:22:08 +0400

    push

--- Frontend/src/pages/admin/courses/management/CourseRoster.tsx
+++ Frontend/src/pages/admin/courses/management/CourseRoster.tsx
@@ -17,11 +17,11 @@ const CourseRoster: React.FC<BookProps> = ({ id }) => {
   const [students, setStudents] = React.useState<UserData[] | null>(null);
   const [unenrolled, setunenrolled] = useState<UserData[] | null>(null);
   const [showModal, setShowModal] = useState(false);
-
+  const [showSecond, setShowSecond] = useState(false); 
   let [selectedStudentId, setSelectedStudentId] = useState<number | null>(null); // State to store selected student's ID
 
   const handleSelectChange = (e: any) => {
-    setSelectedStudentId(e.target.value); 
+    setSelectedStudentId(e.target.value);
   };
   useEffect(() => {
     socket.emit("get-course-roaster", { id: id });
@@ -37,22 +37,45 @@ const CourseRoster: React.FC<BookProps> = ({ id }) => {
     };
   }, []);
   const unenrolledStudents = () => {
-    socket.emit("get-unenrolled-students", {course_id: id});
+    socket.emit("get-unenrolled-students", { course_id: id });
     socket.on("get-unenrolled-students-response", (data) => {
       setunenrolled(data);
       console.log("This ois the unenrolled students", data);
     });
   };
 
-  const handleEnroll = ( student_id: number | null) => {
+  const showInstructors = () => {
+    socket.emit("get-unAssigned-instructors", { course_id: id });
+    socket.on("get-unAssigned-instructors-response", (data) => {
+      setunenrolled(data);
+      console.log("This ois the get-unAssigned-instructors-responses", data);
+    });
+  }
+
+  const handleEnroll = (student_id: number | null) => {
     console.log("this is the student id", student_id);
-    socket.emit("add-student-to-course", {course_id: id, student_id: student_id} );
+    socket.emit("add-student-to-course", {
+      course_id: id,
+      student_id: student_id,
+    });
 
     socket.on("add-student-to-course-response", (data) => {
       console.log("this is the response from the roaster", data);
-    })
-  }
-  
+    });
+  };
+
+  const handleAssign = (student_id: number | null) => {
+    console.log("this is the student id", student_id);
+    socket.emit("add-instructor-to-course", {
+      course_id: id,
+      instructor_id
+: student_id,
+    });
+
+    socket.on("add-instructor-to-course-response", (data) => {
+      console.log("this is the response from the roaster", data);
+    });
+  };
 
   const exportToCSV = () => {
     if (!students) return;
@@ -111,6 +134,31 @@ const CourseRoster: React.FC<BookProps> = ({ id }) => {
                     />
                   </svg>
                   Enroll student
+                </button>
+
+                <button
+                  type="button"
+                  className="inline-flex cursor-pointer items-center rounded-lg border border-gray-400 bg-white py-2 px-3 text-center text-sm font-medium text-gray-800 shadow hover:bg-gray-100 focus:shadow mr-5"
+                  onClick={() => {
+                    setShowSecond(true);
+                    showInstructors();
+                  }}
+                >
+                  <svg
+                    className="mr-1 h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                    />
+                  </svg>
+                  Assign instructor
                 </button>
 
                 <button
@@ -216,47 +264,97 @@ const CourseRoster: React.FC<BookProps> = ({ id }) => {
       </div>
 
       <div>
-      {showModal && (
-        <div className="fixed inset-0 overflow-y-auto z-50 flex justify-center items-center">
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="relative z-10 bg-white p-6 rounded-lg shadow-xl">
-            <h2 className="text-2xl font-bold mb-4">Enroll student</h2>
-            <div className="relative">
-              <select
-                className="block w-full px-4 py-2 bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400 max-h-300 overflow-y-auto"
-                // onChange={handleSelectChange} 
-                value={selectedStudentId?.toString()} 
-              >
-                <option value="">Select a student</option>
-                {unenrolled &&
-                  unenrolled.length > 0 &&
-                  unenrolled.map((student, index) => (
-                    <option key={index} value={student.id} onClick={() => setSelectedStudentId(student.id)}>
-                      {`${student.fName} ${student.lName}`}
-                      {"-"}{selectedStudentId = student.id}
-                    </option>
-                  ))}
-              </select>
-            </div>
-            <div className="flex justify-between mt-4">
-              <button
-                onClick={() => setShowModal(false)}
-                className="px-4 py-2 text-sm font-semibold text-white bg-gray-600 rounded-md hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-500"
-              >
-                Close
-              </button>
-              <button
-                onClick={() => handleEnroll((selectedStudentId))}
-                className="px-4 py-2 text-sm font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500"
-              >
-                Enroll
-              </button>
+        {showModal && (
+          <div className="fixed inset-0 overflow-y-auto z-50 flex justify-center items-center">
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+            <div className="relative z-10 bg-white p-6 rounded-lg shadow-xl">
+              <h2 className="text-2xl font-bold mb-4">Enroll student</h2>
+              <div className="relative">
+                <select
+                  className="block w-full px-4 py-2 bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400 max-h-300 overflow-y-auto"
+                  // onChange={handleSelectChange}
+                  value={selectedStudentId?.toString()}
+                >
+                  <option value="">Select a student</option>
+                  {unenrolled &&
+                    unenrolled.length > 0 &&
+                    unenrolled.map((student, index) => (
+                      <option
+                        key={index}
+                        value={student.id}
+                        onClick={() => setSelectedStudentId(student.id)}
+                      >
+                        {`${student.fName} ${student.lName}`}
+                        {"-"}
+                        {(selectedStudentId = student.id)}
+                      </option>
+                    ))}
+                </select>
+              </div>
+              <div className="flex justify-between mt-4">
+                <button
+                  onClick={() => setShowModal(false)}
+                  className="px-4 py-2 text-sm font-semibold text-white bg-gray-600 rounded-md hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-500"
+                >
+                  Close
+                </button>
+                <button
+                  onClick={() => handleEnroll(selectedStudentId)}
+                  className="px-4 py-2 text-sm font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500"
+                >
+                  Enroll
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
 
-
+      <div>
+        {showSecond && (
+          <div className="fixed inset-0 overflow-y-auto z-50 flex justify-center items-center">
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+            <div className="relative z-10 bg-white p-6 rounded-lg shadow-xl">
+              <h2 className="text-2xl font-bold mb-4">Assign instructors</h2>
+              <div className="relative">
+                <select
+                  className="block w-full px-4 py-2 bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400 max-h-300 overflow-y-auto"
+                  onChange={handleSelectChange}
+                  value={selectedStudentId?.toString()}
+                >
+                  <option value="">Select a instructor</option>
+                  {unenrolled &&
+                    unenrolled.length > 0 &&
+                    unenrolled.map((student, index) => (
+                      <option
+                        key={index}
+                        value={student.id}
+                        onClick={() => setSelectedStudentId(Number(student.id))}
+                      >
+                        {`${student.fName} ${student.lName}`}
+                        {"-"}
+                        {(selectedStudentId = student.id)}
+                      </option>
+                    ))}
+                </select>
+              </div>
+              <div className="flex justify-between mt-4">
+                <button
+                  onClick={() => setShowSecond(false)}
+                  className="px-4 py-2 text-sm font-semibold text-white bg-gray-600 rounded-md hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-500"
+                >
+                  Close
+                </button>
+                <button
+                  onClick={() => handleAssign(selectedStudentId)}
+                  className="px-4 py-2 text-sm font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500"
+                >
+                  Enroll
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
