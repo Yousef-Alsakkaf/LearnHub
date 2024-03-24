a59b296d193cd143bee36617d0c4b1e6761953ec
Abdulrahman Alali <22000577@ug.buid.ac.ae>
2024-03-24 22:58:05 +0400

    5ra

--- Backend/Routes/courses/view-all.ts
+++ Backend/Routes/courses/view-all.ts
@@ -6,37 +6,39 @@ const command = new ServerCommandBuilder("get-all-courses")
   .setAccessLevel(UserAccessLevels.STUDENT)
   .setOutgoingChannel("get-all-courses-response")
   .setIncomingValidationSchema({
-        type: "object",
-        additionalProperties: false,
-        properties: {},       
-      })
+    type: "object",
+    additionalProperties: false,
+    properties: {},
+  })
   .setExecute(callback)
   .setOutgoingValidationSchema({})
   .build();
 
 async function callback({ Client, Data, Database }: CommandExecuteArguments) {
-   
-  const id=Client.getId();
+  const id = Client.getId();
 
-  let type = await Database.executeQuery('SELECT type FROM users WHERE id=?',[id]);
+  let type = await Database.executeQuery("SELECT type FROM users WHERE id=?", [id]);
   type = type[0].type;
 
-  if(type=="student"){
-    const courses = await Database.executeQuery('SELECT courses.id, title, course_code, academic_year, image, description,COUNT(student_id) AS no_of_enrolled FROM courses  LEFT OUTER JOIN studies ON courses.id=course_id WHERE student_id=? GROUP BY courses.id, title, course_code, academic_year, image, description',[id]);
+  if (type == "student") {
+    const courses = await Database.executeQuery(
+      "SELECT courses.id, title, course_code, academic_year, image, description,COUNT(student_id) AS no_of_enrolled FROM courses  LEFT OUTER JOIN studies ON courses.id=course_id WHERE student_id=? GROUP BY courses.id, title, course_code, academic_year, image, description",
+      [id]
+    );
+    return courses;
+  } else if (type == "instructor") {
+    const courses = await Database.executeQuery(
+      "SELECT courses.id, title, course_code, academic_year, image, description FROM courses  LEFT OUTER JOIN teaches ON courses.id=course_id WHERE instructor_id=? GROUP BY courses.id, title, course_code, academic_year, image, description",
+      [id]
+    );
+    return courses;
+  } else {
+    const courses = await Database.executeQuery(
+      "SELECT courses.id, title, course_code, academic_year, image, description,COUNT(student_id) AS no_of_enrolled FROM courses  LEFT OUTER JOIN studies ON courses.id=course_id GROUP BY courses.id, title, course_code, academic_year, image, description",
+      []
+    );
     return courses;
   }
-
-  else if(type=="instructor"){
-    const courses = await Database.executeQuery('SELECT courses.id, title, course_code, academic_year, image, description,COUNT(student_id) AS no_of_enrolled FROM courses  LEFT OUTER JOIN teaches ON courses.id=course_id WHERE instructor_id=? GROUP BY courses.id, title, course_code, academic_year, image, description',[id]);
-    return courses;
-  }
-  else{
-
-    const courses = await Database.executeQuery('SELECT courses.id, title, course_code, academic_year, image, description,COUNT(student_id) AS no_of_enrolled FROM courses  LEFT OUTER JOIN studies ON courses.id=course_id GROUP BY courses.id, title, course_code, academic_year, image, description',[]);
-   return courses;
-
-  }
-  
 }
 
 export default command;
